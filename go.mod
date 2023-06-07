8e9deb2e04d4b73934ed76e7fe15baa0c794179a
Rogerio Maia <rogeriomql@gmail.com>
2023-06-07 07:46:09 -0300

    primeiro commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module rastrix
+
+go 1.20
+
+require (
+	github.com/correiostech/rastro v1.0.1
+	github.com/correiostech/token v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/correiostech/rastro"
+	"github.com/correiostech/token"
+)
+
+var (
+	urlToken  = flag.String("tk", "https://api.correios.com.br/token/v1/autentica", "url para obter token")
+	urlRastro = flag.String("e", "https://api.correios.com.br/srorastro/v1/objetos/", "endpoint de rastro")
+	objeto    = flag.String("o", "AA003039703BR", "objetos a sere rastreados")
+	usuario   = flag.String("u", "12651", "nome de usuario")
+	senha     = flag.String("p", "sroopew", "senha do usuario")
+	tempo     = flag.Int("s", 900, "tempo para execucao, padrão 15 minutos")
+	repetir   = flag.Bool("t", false, "repetir indefinidamente")
+)
+
+func init() {
+	go token.Start(*urlToken, *usuario, *senha)
+}
+
+func formataData(data string) (string, error) {
+	layout := "2006-01-02T15:04:05"
+	t, err := time.Parse(layout, data)
+	if err != nil {
+		return "", err
+	}
+	brLayout := "02/01/2006 15:04:05"
+	brStr := t.Format(brLayout)
+	return brStr, nil
+}
+
+func rastreia() {
+	for {
+		clientRastro, err := rastro.New(*urlRastro, token.Token)
+		if err != nil {
+			log.Println("erro: rastreia objetos 1: " + err.Error())
+		}
+		rastros, err := clientRastro.Rastreia(*objeto, token.Token)
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+		for _, o := range rastros.Objetos {
+			dt, err := formataData(o.Eventos[0].DataHora)
+			if err != nil {
+				log.Println(err.Error())
+			}
+			log.Printf("%s: %s %s %s %s %s",
+				o.CodigoObjeto,
+				o.Eventos[0].Codigo,
+				o.Eventos[0].Tipo,
+				o.Eventos[0].Descricao,
+				o.Eventos[0].Unidade.Nome,
+				dt,
+			)
+		}
+		if !*repetir {
+			os.Exit(0)
+		}
+		time.Sleep(time.Second * time.Duration(*tempo))
+	}
+}
+func main() {
+	flag.Parse()
+	clientToken, err := token.GetToken(*urlToken, *usuario, *senha)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	token.Token = clientToken.Token
+	rastreia()
+}
